48590feba2be2b33b9f4c4f88ccffe463ad3d4d6
Alokjms <educationasyouwant@gmail.com>
2026-03-15 18:01:05 +0530

    Add files via upload
    By using this code you can easily find the sum of two integers

---
+++ SumOfTwoIntegers.py
@@ -0,0 +1,9 @@
+#Here is simple proagramme to sum of Two Integers
+#here simple we take two input by users and print the sum 
+
+num1=int(input("Enter the First integer : "))
+num2=int(input("Enter the Second integer : "))
+
+sum=num1+num2
+
+print(f'the sum of {num1} and {num2} is {sum}')

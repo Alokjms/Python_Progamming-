628b45a4d033c9ae970f0fad963a8d1c136def96
Alokjms <educationasyouwant@gmail.com>
2026-03-18 08:04:06 +0530

    Add files via upload

---
+++ AvrgOfNatural.py
@@ -0,0 +1,8 @@
+#Program to find the Average of first n natural numbers
+
+print("***********************************Welcome**********************************************************")
+n=int(input("Enter the natural Number "))
+
+avg=(n+1)/2
+
+print("the result is :  " ,avg)
---
+++ SumOfNaturalNo.py
@@ -0,0 +1,7 @@
+#to find the sum of  first n natural numbers 
+print("***********************************Welcome**********************************************************")
+n=int(input("Enter the natural Number "))
+
+Sum=(n*(n+1))/2
+
+print("the result is :  " ,Sum)
